4aa065f67e73c5d5e7fda081731e372bc796b3ad
yogeshsahu15379 <yogeshsahu15379@gmail.com>
2025-02-23 10:18:03 +0530

    resolve mesg bug

--- src/component/card/Card.tsx
+++ src/component/card/Card.tsx
@@ -12,32 +12,39 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ heading, availbility, pricing, setCartData }) => {
   const [msg, setMsg] = useState<{ isVisible: boolean; message: string; type: 'success' | 'warning' | 'error' | 'info' }>({isVisible: false, message: '', type: 'success'});
+
+  const handleCloseMessage = () => {
+    setMsg({ ...msg, isVisible: false });
+  };
+
   return (
     <>
-{  msg.isVisible&&  <Message type={msg.type} message={msg.message} visible={true} onClose={() => {}} />
-}    <div className={`card ${availbility === 'Available' ? 'available' : 'unavailable'}`}>
-      <div className="card-header">
-        <h2>{heading}</h2>
-        <h4>{availbility}</h4>
+      {msg.isVisible && <Message type={msg.type} message={msg.message} visible={true} onClose={handleCloseMessage} />}
+      <div className={`card ${availbility === 'Available' ? 'available' : 'unavailable'}`}>
+        <div className="card-header">
+          <h2>{heading}</h2>
+          <h4>{availbility}</h4>
+        </div>
+        <div className="card-body">
+          <p className="pricing"><span className='price'>Price</span> ${pricing}</p>
+          <Button
+            text="Add to Cart"
+            type='button'
+            onClick={() => {
+              // checking if the item is already in the cart
+              setCartData((prevData: any[]) => {
+                if (!prevData.some(item => item.heading === heading && item.availbility === availbility && item.pricing === pricing)) {
+                  setMsg({isVisible: true, message: 'Item successfully added into cart.', type: 'success'});
+                  return [...prevData, { heading, availbility, pricing }];
+                }
+                setMsg({isVisible: true, message: 'Item already present into cart.', type: 'warning'});
+
+                return prevData;
+              });
+            }}
+          />
+        </div>
       </div>
-      <div className="card-body">
-        <p className="pricing"><span className='price'>Price</span> ${pricing}</p>
-        <Button
-          text="Add to Cart"
-          type='button'
-          onClick={() => {
-            // checking if the item is already in the cart
-            setCartData((prevData: any[]) => {
-              if (!prevData.some(item => item.heading === heading && item.availbility === availbility && item.pricing === pricing)) {
-                setMsg({isVisible: true, message: 'Item successfully added into cart.', type: 'success'});
-                return [...prevData, { heading, availbility, pricing }];
-              }
-              return prevData;
-            });
-          }}
-        />
-      </div>
-    </div>
     </>
   );
 };
--- src/component/form/Form.tsx
+++ src/component/form/Form.tsx
@@ -1,47 +1,72 @@
-import React, { useState } from 'react';
-import Input from '../input/input';
-import generateDomainData from '../../utils/generateDomainData';
+import React, { useState } from "react";
+import Input from "../input/input";
+import generateDomainData from "../../utils/generateDomainData";
 import "./Form.css";
+import Message from "../message/Message";
+
 interface FormProps {
-    setSuggestedData: (data: any) => void;
+  setSuggestedData: (data: any) => void;
 }
 
 const Form: React.FC<FormProps> = ({ setSuggestedData }) => {
-    const [formData, setFormData] = useState({
-        domain: '',
+  const [formData, setFormData] = useState({
+    domain: "",
+  });
+  const [msg, setMsg] = useState<{
+    isVisible: boolean;
+    message: string;
+    type: "success" | "warning" | "error" | "info";
+  }>({ isVisible: false, message: "", type: "success" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
+  };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (formData.domain === "") {
+      setMsg({
+        isVisible: true,
+        message: "Please enter Domain.",
+        type: "error",
+      });
+      return;
+    }
+    setSuggestedData(generateDomainData(formData.domain));
+  };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if(formData.domain === '') {
-            alert('Please enter a domain name');
-            return;
-        }
-        setSuggestedData(generateDomainData(formData.domain));
+  const handleCloseMessage = () => {
+    setMsg({ ...msg, isVisible: false });
+  };
 
-};
-
-    return (
-        <form onSubmit={handleSubmit} className='form-container card-conatiner'>
-            <Input
-                type="text"
-                name="domain"
-                value={formData?.domain}
-                onChange={handleChange}
-                placeholder="Type you dream domain."
-            />
-                <button><span>Search</span> </button>
-
-        </form>
-    );
+  return (
+    <>
+      {msg.isVisible && (
+        <Message
+          type={msg.type}
+          message={msg.message}
+          visible={true}
+          onClose={handleCloseMessage}
+        />
+      )}
+      <form onSubmit={handleSubmit} className="form-container card-conatiner">
+        <Input
+          type="text"
+          name="domain"
+          value={formData?.domain}
+          onChange={handleChange}
+          placeholder="Type you dream domain."
+        />
+        <button>
+          <span>Search</span>{" "}
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Form;
